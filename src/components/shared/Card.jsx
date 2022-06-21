6f451e3e7da8af91bdc1c356ff6ddf8326c53901
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-06-21 11:16:38 +0300

    card fade

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,19 +1,24 @@
-import { FaTimes } from "react-icons/fa";
 import React from "react";
 import PropTypes from "prop-types";
 
-function Card({ children, reverse }) {
+function Card({ children, fadeIn, fadeOut }) {
   // children is a prop that is passed in from the parent component (e.g. FeedbackItem) and is displayed inside of the Card component
-  return <div className={`card ${reverse && "reverse"}`}>{children}</div>; //use the reverse style, if reverse-prop is passed as "true"
+  return (
+    <div className={`card ${fadeIn && "fade-in"} ${fadeOut && "fade-out"}`}>
+      {children}
+    </div>
+  ); //use the reverse style, if reverse-prop is passed as "true"
 }
 
 export default Card;
 
 Card.defaultProps = {
-  reverse: false,
+  fadeIn: false,
+  fadeOut: false,
 };
 
 Card.propTypes = {
   children: PropTypes.node.isRequired,
-  reverse: PropTypes.bool,
+  fadeIn: PropTypes.bool,
+  fadeOut: PropTypes.bool,
 };
