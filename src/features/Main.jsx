f82b84d79eab8a7e6f4065df7671f2485ed6a1c7
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-07-20 18:58:14 +0300

    update

--- src/features/Main.jsx
+++ src/features/Main.jsx
@@ -2,9 +2,39 @@ import React from "react";
 
 function Main() {
   return (
-    <div className="container">
+    <div className="container main">
       <div className="card">
         <h1>Welcome</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nisi
+          corporis. Unde laudantium voluptatibus numquam itaque labore ipsa,
+          nostrum vel illo obcaecati. <a>Random Link, yay.</a> Fugit, adipisci
+          inventore nam saepe architecto sint tenetur.
+        </p>
+        <button className="btn btn-cta">Call To Action!</button>
+        <h2>Subtitle</h2>
+        <img src="https://picsum.photos/201" alt="random image" />
+
+        <a>This is a link that is very long, wheeeeeeee</a>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
+          debitis dolore eveniet fugiat eligendi! Nisi expedita, maiores amet
+          tempora numquam aspernatur alias culpa, minus, et officia aut. Vitae,
+          enim ea.
+        </p>
+        <h3>Another title</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          deleniti eveniet sunt praesentium. Sit ut est iste, nihil dignissimos
+          officia aspernatur non temporibus minima impedit iure aut, corrupti
+          ipsum saepe. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Voluptas facilis harum quaerat veniam, eos officia, illo quis rem
+          perferendis, officiis nostrum eveniet velit odio sunt quam sint
+          dolorum est id.
+        </p>
+        <button className="btn btn-secondary"> button 1</button>
+        <button className="btn btn-secondary"> button 2</button>
+        <button className="btn btn-secondary"> button 3</button>
       </div>
     </div>
   );
