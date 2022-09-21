49d3c7d9d032fce4a7945131bd7daf605b4988b7
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-09-21 09:45:01 +0300

    Create user.js

---
+++ src/modules/user.js
@@ -0,0 +1,37 @@
+const User = (function () {
+  let id = "";
+  let name = "";
+  let authorized = false;
+
+  const isAuthorized = () => {
+    return authorized;
+  };
+
+  const getData = () => {
+    return { id, name, authorized };
+  };
+
+  const getName = () => {
+    return name;
+  };
+
+  const getToken = () => {
+    return JSON.parse(localStorage.getItem("token"));
+  };
+
+  const setData = (data) => {
+    id = data.id || "";
+    name = data.name || "";
+    authorized = data.authorized || false;
+  };
+
+  return {
+    getData,
+    getToken,
+    getName,
+    setData,
+    isAuthorized,
+  };
+})();
+
+export default User;
