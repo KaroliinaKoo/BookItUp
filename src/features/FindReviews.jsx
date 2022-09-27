1444c22fb325a23015c488c82aca722bbcf098bc
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-09-27 16:46:23 +0300

    sort-by

--- src/features/FindReviews.jsx
+++ src/features/FindReviews.jsx
@@ -10,11 +10,25 @@ function FindReview() {
     username: "",
   });
 
-  console.log(search);
+  const [sortBy, setSortBy] = useState("newest");
 
   return (
     <div className="container">
       <ReviewSearch setSearch={setSearch} />
+      <div className="sort-by">
+        <label htmlFor="sort">Sort by:</label>
+        <select
+          name="sort"
+          id="sort"
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+        >
+          <option value="newest">Newest</option>
+          <option value="oldest">Oldest</option>
+          <option value="highest">Highest Rating</option>
+          <option value="lowest">Lowest Rating</option>
+        </select>
+      </div>
       <FeedbackList search={search} />
     </div>
   );
