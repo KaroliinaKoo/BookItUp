0c72c1d11828e5a6ef98ffa3b39b52379ec06671
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-09-29 21:19:53 +0300

    register validation

--- src/features/Login.jsx
+++ src/features/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import User from "../modules/user";
 import { useNavigate } from "react-router-dom";
@@ -8,15 +8,71 @@ function Login() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     email: "",
-    password: "",
     username: "",
+    password: "",
   });
 
+  const [error, setError] = useState({
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [register, setRegister] = useState(false);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+    if (!register) {
+      return;
+    }
+    let { name, value } = e.target;
+    let errorObj = { ...error };
+    switch (name) {
+      case "email":
+        if (!value) {
+          errorObj.email = "Email is required";
+        } else if (!/\S+@\S+\.\S+/.test(value)) {
+          errorObj.email = "Email is invalid";
+        } else {
+          errorObj.email = "";
+        }
+        break;
+
+      case "username":
+        if (!value) {
+          errorObj.username = "Username is required";
+        } else {
+          errorObj.username = "";
+        }
+        break;
+
+      case "password":
+        if (!value) {
+          errorObj.password = "Password is required";
+        } else if (value.length < 6) {
+          errorObj.password = "Password must be at least 6 characters";
+        } else {
+          errorObj.password = "";
+        }
+        break;
+
+      case "confirmPassword":
+        if (!value) {
+          errorObj.confirmPassword = "Confirm password is required";
+        } else if (value !== formData.password) {
+          errorObj.confirmPassword = "Passwords do not match";
+        } else {
+          errorObj.confirmPassword = "";
+        }
+        break;
+
+      default:
+        break;
+    }
+    setError(errorObj);
   };
 
   const handleSubmit = (e) => {
@@ -97,8 +153,28 @@ function Login() {
               required
               autoFocus
             />
+            {register && error.email && <p className="error">{error.email}</p>}
           </div>
-
+          {register && (
+            <div className="input-group">
+              <label htmlFor="username">Username</label>
+              <input
+                value={formData.username}
+                onChange={handleChange}
+                type="username"
+                id="username"
+                name="username"
+                placeholder="Your username"
+                autoComplete="on"
+                maxLength={18}
+                minLength={3}
+                required
+              />
+              {register && error.username && (
+                <p className="error">{error.username}</p>
+              )}
+            </div>
+          )}
           <div className="input-group">
             <label htmlFor="password">Password</label>
             <input
@@ -120,23 +196,36 @@ function Login() {
             >
               {showPassword ? <FaEyeSlash /> : <FaEye />}
             </button>
+            {register && error.password && (
+              <p className="error">{error.password}</p>
+            )}
           </div>
 
           {register && (
             <div className="input-group">
-              <label htmlFor="username">Username</label>
+              <label htmlFor="confirm-password">Confirm Password</label>
               <input
-                value={formData.username}
-                onChange={handleChange}
-                type="username"
-                id="username"
-                name="username"
-                placeholder="Your username"
-                autoComplete="on"
-                maxLength={18}
-                minLength={3}
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                type={showPassword ? "text" : "password"}
+                id="confirmPassword"
+                name="confirm-password"
+                placeholder="Write your password again"
+                autoComplete="off"
+                maxLength={32}
+                minLength={8}
                 required
               />
+              <button
+                type="button"
+                className="show-password"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+              {register && error.confirmPassword && (
+                <p className="error">{error.confirmPassword}</p>
+              )}
             </div>
           )}
 
