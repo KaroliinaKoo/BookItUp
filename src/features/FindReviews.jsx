e5a5570f1f4fc028536e89016d43d45b3ab6759d
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-09-30 15:28:03 +0300

    scroll-to-top

--- src/features/FindReviews.jsx
+++ src/features/FindReviews.jsx
@@ -1,6 +1,8 @@
 import FeedbackList from "../components/FeedbackList";
 import ReviewSearch from "../components/ReviewSearch";
 import { useState } from "react";
+import { FaChevronUp } from "react-icons/fa";
+import { useEffect } from "react";
 
 function FindReview() {
   const [search, setSearch] = useState({
@@ -11,9 +13,27 @@ function FindReview() {
   });
 
   const [sortBy, setSortBy] = useState("newest");
+  const [showScroll, setShowScroll] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const scrollEvent = (e) => {
+    setScrollPosition(e.target.scrollTop);
+    if (e.target.scrollTop > 300) {
+      setShowScroll(true);
+    } else {
+      setShowScroll(false);
+    }
+  };
+
+  const scrollTop = () => {
+    document.getElementsByClassName("container")[0].scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   return (
-    <div className="container">
+    <div className="container" onScroll={scrollEvent}>
       <h1>Find Reviews</h1>
       <ReviewSearch setSearch={setSearch} />
       <div className="sort-by">
@@ -31,6 +51,11 @@ function FindReview() {
         </select>
       </div>
       <FeedbackList search={search} sortBy={sortBy} />
+      {showScroll && (
+        <button className="scroll-to-top" onClick={scrollTop}>
+          <FaChevronUp />
+        </button>
+      )}
     </div>
   );
 }
