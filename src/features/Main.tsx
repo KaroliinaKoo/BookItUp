ed7e036aa16ad1918edc0413f325deb76fd9da72
karoliina-koskelainen <karoliina.koskelainen@gmail.com>
2022-11-28 17:21:18 +0200

    update

--- src/features/Main.tsx
+++ src/features/Main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import User from "../modules/user";
+import Recommendations from "../components/Recommendations";
 
 function Main() {
   return (
-    <div className="hero-container">
+    <div className="main-container">
       <div className="hero-card">
         <p>The Best Reads</p> <p>Wherever you are.</p>
         {User?.isAuthorized() ? (
@@ -14,12 +15,14 @@ function Main() {
             Sign Up Today
           </NavLink>
         )}
-        <div className="credit">
-          <a href="https://unsplash.com/@patriotbarrow?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-            Photo by Dan Dumitriu
-          </a>
-        </div>
+        <a
+          className="credit"
+          href="https://unsplash.com/@patriotbarrow?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+        >
+          Photo by Dan Dumitriu
+        </a>
       </div>
+      <Recommendations />
     </div>
   );
 }
